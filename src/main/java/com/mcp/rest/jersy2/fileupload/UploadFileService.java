88721da79f1e9fcb711befc2344fa47884e17adc
cm1 <chowdappa_m@intuit.com>
2017-12-14 11:19:13 +0530

    added file upload service

---
+++ src/main/java/com/mcp/rest/jersy2/fileupload/UploadFileService.java
@@ -0,0 +1,34 @@
+package com.mcp.rest.jersy2.fileupload;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+
+@Path("/file")
+public class UploadFileService {
+
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+		
+		return Response.status(200).build();
+
+	}
+
+}
